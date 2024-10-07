6208bf29454482d6c7c337e4f1d0d0e49f9c3152
lorrainestorme <140294111+lorrainestorme@users.noreply.github.com>
2024-10-07 01:22:50 -0400

    Cover page fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex w-screen h-screen items-center justify-center">
+    <div className="flex-col w-screen h-screen items-center justify-center">
         <p className="text-5xl">smcs alumni collective yay!</p>
         <div className="flex justify-between">
-            <div className="w-[1/2]">
+            <div className="w-1/2">
                 <p className="text-5xl text-[#ff3130] uppercase">Connecting</p>
                 <p className="text-4xl">the community of SMCS alumni together for networking.</p>
             </div>
-            <div className="w-[1/2]">
+            <div className="w-1/2">
                 <p className="text-5xl text-[#ff3130] uppercase">Fundraising</p>
                 <p className="text-4xl">to support ongoing activities for the benefit of current SMCS students.</p>
             </div>
