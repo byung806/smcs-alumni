987c0c5a0ae7c1c132cd914a3cc7e9232576d9df
lorrainestorme <lorrniaba@gmail.com>
2025-01-06 23:39:12 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
 import HeroSection from "@/components/HeroSection";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-col w-screen h-screen items-center justify-center">
+        <HeroSection type="home" />
+        <div className="flex justify-between">
+            <div className="w-1/2">
+                <p className="text-5xl text-[#ff3130] uppercase">Connecting</p>
+                <p className="text-4xl">the community of SMCS alumni together for networking.</p>
+            </div>
+            <div className="w-1/2">
+                <p className="text-5xl text-[#ff3130] uppercase">Fundraising</p>
+                <p className="text-4xl">to support ongoing activities for the benefit of current SMCS students.</p>
+            </div>
+        </div>
+    </div>
+    // <div className="flex w-screen h-screen items-center justify-center">
+    //   <p className="text-4xl">smcs alumni collective yay!</p>
+    // </div>
+  );
+}
+import HeroSection from "@/components/HeroSection";
 import Content from "@/components/Content";
 import Link from "next/link";
 
