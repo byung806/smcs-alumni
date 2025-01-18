20fb529f26a1016de67e892a9d411f6b2da21d3a
Bryan Yung <byung806@gmail.com>
2025-01-17 19:01:50 -0500

    edit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
 
 const nextConfig = {
     basePath: "",
     output: "export",  // <=== enables static exports
-    images: { unoptimized: true },
+    reactStrictMode: true,
+    images: { unoptimized: true }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
